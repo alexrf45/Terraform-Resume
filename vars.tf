c9c4b8c84253b73f5d3c2ba27ced7b437f848f03
alexrf45 <fonalex45@gmail.com>
2023-02-01 23:31:32 -0500

    configured terraform autocompletion. not fun. need to figure out how to get the rest of the resource completion to work

---
+++ vars.tf
@@ -0,0 +1,10 @@
+#variable "server_port"{
+#	description = "The port the server will use for HTTP requests"
+#	type        = number
+#}
+
+#variable "test" {
+#	description = "test variable with autocompletion"
+#	type        = string
+#}
+
