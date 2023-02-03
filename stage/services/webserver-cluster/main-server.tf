72e97141aae0be089952cc935ef6ded932a4ac48
alexrf45 <fonalex45@gmail.com>
2023-02-03 17:38:33 -0500

    - db set up - remote data_source - webserver displaying remote_data source

--- stage/services/webserver-cluster/main-server.tf
+++ stage/services/webserver-cluster/main-server.tf
@@ -1,12 +1,21 @@
+data "terraform_remote_state" "db" {
+  backend = "s3"
+  config = {
+    bucket = "tf-state-resume"
+    key = "stage/data-stores/mysql/terraform.tfstate"
+    region = "us-east-2"
+  }
+}
+
 resource "aws_launch_configuration" "example" {
   image_id = "ami-0fb653ca2d3203ac1"
   instance_type = "t2.micro"
   security_groups = [aws_security_group.instance.id]
-  user_data = <<-EOF
-							#!/bin/bash
-							echo "Hello, World" > index.html
-							nohup busybox httpd -f -p ${var.server_port} &
-							EOF
+  user_data = templatefile("user-data.sh", {
+    server_port = var.server_port
+    db_address  = data.terraform_remote_state.db.outputs.address
+    db_port     = data.terraform_remote_state.db.outputs.port
+  })
 	lifecycle {
 		create_before_destroy = true
 	}
